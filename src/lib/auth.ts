82aea81548424d7c47e566e1c102ff7a23e87391
hecsolmay <hecsolmay@gmail.com>
2025-06-25 13:06:10 -0600

    refactor: replace hardcoded user key with constant for local storage operations

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,22 @@
 import type { User } from '@/types/user'
 
+const USER_LOCAL_STORAGE_KEY = 'user'
+
 export function getUser (): User | null {
-  const user = localStorage.getItem('user')
+  const user = localStorage.getItem(USER_LOCAL_STORAGE_KEY)
 
   if (!user) {
     return null
   }
 
-  // TODO: validar el formato del objeto
+  // Para mejoras en el futuro se validará el formato del objeto
   return JSON.parse(user)
+}
+
+export function saveLocalStorageUser (user: User) {
+  localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(user))
+}
+
+export function removeLocalStorageUser () {
+  localStorage.removeItem(USER_LOCAL_STORAGE_KEY)
 }

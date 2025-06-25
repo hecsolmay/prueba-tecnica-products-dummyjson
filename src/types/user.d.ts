0e59d2e62e8b00f476f34a7768405fee3cb4dcc7
hecsolmay <hecsolmay@gmail.com>
2025-06-25 13:06:38 -0600

    feat: update User interface to include detailed user properties

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,4 +1,9 @@
 export interface User {
-  id: string
-  name: string
+  id: number
+  username: string
+  email: string
+  firstName: string
+  lastName: string
+  gender: string
+  image: string
 }

9903e4c85aba70b8ae9a691cb8333d7761dfe415
hecsolmay <hecsolmay@gmail.com>
2025-06-25 20:57:18 -0600

    feat: refactor HomePage layout to include ProductFormProvider and improve structure

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,24 @@
+import CreateProductButton from '@/components/products/CreateProductButton'
+import ProductFormModal from '@/components/products/ProductFormModal'
 import ProductsFilters from '@/components/products/ProductsFilters'
 import ProductsList from '@/components/products/productsList'
+import { ProductFormProvider } from '@/context/ProductFormContext'
 
 export default function HomePage () {
   return (
-    <>
-      <h1 className='mb-6 text-2xl font-semibold'>Conoce nuestros productos</h1>
+    <ProductFormProvider>
+      <div className='flex flex-wrap items-center justify-between gap-4'>
+        <h1 className='mb-6 text-2xl font-semibold'>
+          Conoce nuestros productos
+        </h1>
+        <CreateProductButton />
+      </div>
+
+      {/* Product Form Modal */}
+      <ProductFormModal />
+
       <ProductsFilters />
       <ProductsList />
-    </>
+    </ProductFormProvider>
   )
 }
